d64e88fcfda43c178bd0b6f7024e36747bbd8072
Reiq.Laptop <reiqvideos@gmail.com>
2021-11-12 00:27:28 -0400

    Estado inicial e imágenes cargadas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Component } from "react";
+import Main from "./views/main";
 
-
-function App() {
-  return (
-    <h1>Hola</h1>
-  );
+class App extends Component {
+  state = {
+    products: [
+      { name: 'Silla', price: 6000, img: '/products/silla.jpg' },
+      { name: 'Audífonos', price: 1500, img: '/products/audifonos.jpg' },
+      { name: 'Monitor', price: 13000, img: '/products/monitor.jpg' },
+      { name: 'Mouse', price: 350, img: '/products/raton.jpg' },
+      { name: 'Teclado', price: 1200, img: '/products/teclado.jpg' }
+    ]
+  }
+  render() {
+    return (
+      <Main />
+    )
+  }
 }
 
 export default App;
---
+++ src/views/main.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+    render() {
+        return(
+            <React.Fragment>
+                <h1>Welcome</h1>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Main;
