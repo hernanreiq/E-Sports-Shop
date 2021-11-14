e5e74da41b4a2318e91dd6108326a3ce12e6c592
Reiq.Laptop <reiqvideos@gmail.com>
2021-11-14 04:24:19 +0100

    Estilización de los botones para agregar, restar y eliminar productos del carrito

--- src/components/cart-details.jsx
+++ src/components/cart-details.jsx
@@ -1,5 +1,7 @@
 import { Component } from "react";
 import AmountPayable from "./amount-payable";
+import { faTrashAlt, faMinusCircle, faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class CartDetails extends Component {
     render() {
@@ -9,14 +11,25 @@ class CartDetails extends Component {
                 {cart.map((product, i) => {
                     return (
                         <div className="card" key={i}>
-                            <div className="card-header bg-dark p-2">
-                                <p className="mb-0 text-white">
-                                    <img src={product.img} alt={product.name} className="cart-img-product mr-2" />
-                                    {product.name}
-                                    <span className="badge badge-success ml-1">
-                                        {product.quantity}
-                                    </span>
-                                </p>
+                            <div className="card-body bg-dark p-2">
+                                <div className="row w-100 m-0">
+                                    <div className="p-0 col-11">
+                                        <p className="mb-0 text-white">
+                                            <img src={product.img} alt={product.name} className="cart-img-product mr-2" />
+                                            {product.name}
+                                            <span className="badge badge-success ml-1">
+                                                {product.quantity}
+                                            </span>
+                                        </p>
+                                    </div>
+                                    <div className="p-0 col-1 text-center">
+                                        {product.quantity > 1 ?
+                                            <FontAwesomeIcon icon={faMinusCircle} className="text-danger cart-button-product" /> :
+                                            <FontAwesomeIcon icon={faTrashAlt} className="text-danger cart-button-product" />
+                                        }
+                                        <FontAwesomeIcon icon={faPlusCircle} className="text-success cart-button-product" />
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     )
